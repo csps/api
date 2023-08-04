5fb694655cc3803cdc978cde2d0f93f2fb724f63
TotalElderBerry <lisondrabrian48@gmail.com>
2023-08-04 15:54:48 +0800

    (feat): added GET /announcements, GET DELETE /announcements/:academic_year/academic_year.

--- src/core/announcements.ts
+++ src/core/announcements.ts
@@ -24,6 +24,7 @@ export function announcements(request: Request, response: Response) {
       case 'PUT':
         break;
       case 'DELETE':
+        deleteAnnouncement(request,response);
         break;
     }
   }
