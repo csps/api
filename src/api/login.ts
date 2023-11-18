bc90b5c14e93ce92a635e2af3ead83a5a5f6eec2
mavyfaby <maverickfabroa@gmail.com>
2023-11-19 00:39:44 +0800

    feat(login): return status 204 instead of 400 - chore(app): update dependencies

--- src/api/login.ts
+++ src/api/login.ts
@@ -35,7 +35,7 @@ async function getLogin(context: ElysiaContext) {
 
   // If token is not specified
   if (!token) {
-    context.set.status = 400;
+    context.set.status = 204;
     return response.error(Strings.GENERAL_NO_SESSION);
   }
 
