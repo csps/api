0f1c495fac1924d3ed7e91e89399d836b6e1bd21
mavyfaby <maverickfabroa@gmail.com>
2023-11-30 20:54:05 +0800

    feat(routes): add auth for `/events/:id`

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -15,52 +15,40 @@ import reset from "../api/reset";
 import env from "../api/env";
 
 const routes: AppRoutes[] = [
-  // Example
   { path: "/example", methods: ["GET", "POST", "DELETE", "PUT"], handler: example },
 
-  // Env
-  { path: "/env/:key", methods: ["GET", "PUT", "DELETE"], handler: env, auth: { "PUT": AuthType.ADMIN, "DELETE": AuthType.ADMIN }},
-  { path: "/env", methods: ["GET", "POST"], handler: env, auth: { "POST": AuthType.ADMIN }},
+  { path: "/announcements/:id", methods: ["PUT", "DELETE", "OPTIONS"], handler: announcements, auth: { PUT: AuthType.ADMIN, DELETE: AuthType.ADMIN }},
+  { path: "/announcements", methods: ["GET","POST"], handler: announcements, auth: { POST: AuthType.ADMIN }},
 
-  // Courses
-  { path: "/courses/:id", methods: ["PUT", "DELETE"], handler: courses, auth: { "PUT": AuthType.ADMIN, "DELETE": AuthType.ADMIN } },
-  { path: "/courses", methods: ["GET", "POST"], handler: courses, auth: { "POST": AuthType.ADMIN }},
+  { path: "/courses/:id", methods: ["PUT", "DELETE"], handler: courses, auth: { PUT: AuthType.ADMIN, DELETE: AuthType.ADMIN } },
+  { path: "/courses", methods: ["GET", "POST"], handler: courses, auth: { POST: AuthType.ADMIN }},
 
-  // Forgot password
+  { path: "/env/:key", methods: ["GET", "PUT", "DELETE"], handler: env, auth: { PUT: AuthType.ADMIN, DELETE: AuthType.ADMIN }},
+  { path: "/env", methods: ["GET", "POST"], handler: env, auth: { POST: AuthType.ADMIN }},
+
+  { path: "/events/next", methods: ["GET"], handler: events },
+  { path: "/events/:id", methods: ["PUT", "DELETE", "OPTIONS"], handler: events, auth: { PUT: AuthType.ADMIN, DELETE: AuthType.ADMIN  }},
+  { path: "/events", methods: ["GET", "POST"], handler: events, auth: { POST: AuthType.ADMIN }},
+
   { path: "/forgot", methods: ["POST"], handler: forgot },
 
-  // Announcements
-  { path: "/announcements/:id", methods: ["PUT", "DELETE", "OPTIONS"], handler: announcements, auth: { "PUT": AuthType.ADMIN, "DELETE": AuthType.ADMIN }},
-  { path: "/announcements", methods: ["GET","POST"], handler: announcements, auth: { "POST": AuthType.ADMIN }},
-
-  // Events
-  { path: "/events/next", methods: ["GET"], handler: events },
-  { path: "/events/:id", methods: ["PUT", "DELETE"], handler: events },
-  { path: "/events", methods: ["GET", "POST"], handler: events, auth: { "POST": AuthType.ADMIN }},
-
-  // Students
-  { path: "/students/:student_id", methods: ["PUT"], handler: students, auth: { "POST": AuthType.ADMIN }},
-  { path: "/students", methods: ["GET", "POST"], handler: students, auth: { "POST": AuthType.ADMIN }},
-
-  // Login
   { path: "/login", methods: ["GET", "POST", "OPTIONS"], handler: login },
 
-  // Photos
+  { path: "/orders/:id/:key", methods: ["PUT", "OPTIONS"], handler: orders, auth: { PUT: AuthType.ADMIN }},
+  { path: "/orders/reference/:reference", methods: ["GET"], handler: orders, auth: { "GET": AuthType.ADMIN }},
+  { path: "/orders", methods: ["GET", "POST"], handler: orders },
+  
   { path: "/photos/:hash", methods: ["GET"], handler: photos },
   { path: "/photos", methods: ["POST"], handler: photos },
 
-  // Products
   { path: "/products/:slug", methods: ["GET"], handler: products },
   { path: "/products", methods: ["GET"], handler: products },
+  
+  { path: "/students/:student_id", methods: ["PUT"], handler: students, auth: { POST: AuthType.ADMIN }},
+  { path: "/students", methods: ["GET", "POST"], handler: students, auth: { POST: AuthType.ADMIN }},
 
-  // Reset password
   { path: "/reset/:token", methods: ["GET"], handler: reset },
   { path: "/reset/", methods: ["POST"], handler: reset },
-
-  // Orders
-  { path: "/orders/:id/:key", methods: ["PUT", "OPTIONS"], handler: orders, auth: { "PUT": AuthType.ADMIN }},
-  { path: "/orders/reference/:reference", methods: ["GET"], handler: orders, auth: { "GET": AuthType.ADMIN }},
-  { path: "/orders", methods: ["GET", "POST"], handler: orders },
 ];
 
 export function status404(context: ElysiaContext): ResponseBody {
