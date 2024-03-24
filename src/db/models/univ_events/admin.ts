d84d8d075b34ca60a2062bdbbc86d112c61240fb
TotalElderBerry <lisondrabrian48@gmail.com>
2024-03-24 12:47:51 +0800

    fix(tatak): added college details after admin login

--- src/db/models/univ_events/admin.ts
+++ src/db/models/univ_events/admin.ts
@@ -27,7 +27,7 @@ class UnivAdmin {
       try {
         // Get admin by username and password
         const result = await db.query<AdminData[]>(
-          "SELECT s.id, s.student_id, s.last_name, s.first_name, s.year_level, s.email_address, s.password, s.date_stamp, s.course_id FROM univ_admin a INNER JOIN univ_students s ON s.id = a.univstudents_id WHERE s.student_id = ?", [username]
+          "SELECT s.id, s.student_id, s.last_name, s.first_name, s.year_level, s.email_address, s.password, s.date_stamp, s.course_id, co.acronym, co.name FROM univ_admin a INNER JOIN univ_students s ON s.id = a.univstudents_id inner join colleges_courses c ON c.id = s.course_id inner join colleges co on co.id = c.college_id WHERE s.student_id = ?", [username]
         );
 
         // If no result or password is incorrect
@@ -57,9 +57,9 @@ class UnivAdmin {
       try {
         // Get admin by username
         const result = await db.query<AdminData[]>(
-          "SELECT s.id, s.student_id, s.last_name, s.first_name, s.year_level, s.email_address, s.password, s.date_stamp, s.course_id FROM univ_admin a INNER JOIN univ_students s ON s.id = a.univstudents_id WHERE s.student_id = ?", [username]
+          "SELECT s.id, s.student_id, s.last_name, s.first_name, s.year_level, s.email_address, s.password, s.date_stamp, s.course_id, co.acronym, co.name FROM univ_admin a INNER JOIN univ_students s ON s.id = a.univstudents_id inner join colleges_courses c ON c.id = s.course_id inner join colleges co on co.id = c.college_id WHERE s.student_id = ?", [username]
         );
-
+       
         if (result.length === 0) {
           return reject("UC Days Admin not found");
         }
